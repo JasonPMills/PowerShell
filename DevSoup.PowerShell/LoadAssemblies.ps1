ba26cb71a092cca4fc9e5549613cdf974d9b039b
JasonPMills <JasonPMills@hotmail.com>
2015-01-23 12:20:04 -0500

    Created Project

---
+++ DevSoup.PowerShell/LoadAssemblies.ps1
@@ -0,0 +1,19 @@
+﻿
+function Load-DLL($PartialName){
+	Write-Host "Loading" $PartialName
+	$loadDll = [System.Reflection.Assembly]::LoadWithPartialName($PartialName);
+	return $loadDll -ne $null
+}
+
+function Add-Assemblies($assemblies)
+{		
+	$assemblies | Foreach-Object {
+		$result = Load-DLL $_
+		if($result -eq $null){
+			Write-Host "Error Loading" $_
+		}
+	}
+}
+
+$assemblies = "Microsoft.SharePoint.Client", "Microsoft.SharePoint.Client.Runtime", "Microsoft.SharePoint.Client.DocumentManagement.dll";
+Add-Assemblies $assemblies
